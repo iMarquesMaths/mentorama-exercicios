7f7050865bd2a01aacd79d47a6a1bf1da8fc7638
matheus <matheus.1102@hotmail.com>
2020-12-28 16:11:13 -0300

    clean syntax

--- modulo-06/Ex001.py
+++ modulo-06/Ex001.py
@@ -1,16 +1,17 @@
 import sqlite3
 
-#Conecta o banco de dados com a memoria do pc (reinicia quando recarrega)
+# Conecta o banco de dados com a memoria do pc (reinicia quando recarrega)
 database = sqlite3.connect(':memory:')
 
-#Conecta o banco de dados criando um arquivo no pc (da conflito caso o banco ja exista)
-#database = sqlite3.connect('teste.db')
+# Conecta o banco de dados criando um arquivo no pc (da conflito caso o banco ja exista)
+# database = sqlite3.connect('teste.db')
 
-#define um cursor para interagir com o banco de dados
+# define um cursor para interagir com o banco de dados
 cursor = database.cursor()
 
-#função para criação da tabela funcionarios
-def create_table_funcionarios:
+
+# função para criação da tabela funcionarios
+def create_table_employees():
     sql = """
     CREATE TABLE IF NOT EXISTS employees (
     idEmployees INTEGER NOT NULL PRIMARY KEY,
